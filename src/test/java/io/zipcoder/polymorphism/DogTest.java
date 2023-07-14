b4a2c31d4d92c5f4c3a2132b7604ccfb50eaa30c
Joe <Jhuntenburg@gmail.com>
2023-07-14 10:40:31 -0400

    Dog tests added

--- src/test/java/io/zipcoder/polymorphism/DogTest.java
+++ src/test/java/io/zipcoder/polymorphism/DogTest.java
@@ -1,7 +1,43 @@
 package io.zipcoder.polymorphism;
 
 import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class DogTest extends TestCase {
 
+    @Test
+    public void speakTest() {
+//        Given
+        Dog d = new Dog();
+        String expected = "Woof!";
+
+//        When
+        String actual = d.speak();
+//        Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetName() {
+//        Given
+       Dog d = new Dog();
+        String expected = "Fido";
+//        When
+        String actual = d.getName();
+//        Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetName() {
+//        Given
+        Dog d = new Dog();
+        String expected = "Whiskers";
+//        When
+        d.setName(expected);
+        String actual = d.getName();
+//        Then
+        Assert.assertEquals(expected, actual);
+    }
 }
