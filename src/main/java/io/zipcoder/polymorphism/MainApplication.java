6573744a5919744dd061f52b51f5d0e32b3a5e78
Joe <Jhuntenburg@gmail.com>
2023-07-16 13:19:44 -0400

    part 3 starting to work

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -2,16 +2,17 @@ package io.zipcoder.polymorphism;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
-public class MainApplication {
+public abstract class MainApplication extends Pet {
     public static void main(String[] args) {
         getUserInput();
     }
 
     static void getUserInput() {
-        Scanner scanner = new Scanner(System.in);
-        int numberOfPetsUserHas = 0;
+     /*    Scanner scanner = new Scanner(System.in);
+       int numberOfPetsUserHas = 0;
 
         List<String> petNames = new ArrayList();
         List<String> petAges = new ArrayList();
@@ -34,9 +35,45 @@ public class MainApplication {
             int index = count - 1;
             System.out.println("Pet #" + count + " - Name: " + name + ", Age: " + petAges.get(index));
             count++;
+        }*/
+        List<Pet> petList = new ArrayList<>();
+        int numberOfPets;
+        String petType;
+        String petName;
+        Scanner numberOfPetsScanner = new Scanner(System.in);
+        Scanner petTypeScanner = new Scanner(System.in);
+        Scanner petNameScanner = new Scanner(System.in);
+        System.out.print("HOW MANY PETS DO YOU HAVE?  ");
+        numberOfPets = numberOfPetsScanner.nextInt();
+        for (int i = 1; i <= numberOfPets; i++) {
+            System.out.println("WHAT KIND OF PET IS PET " + i + "?\n" +
+                    "cat, dog, or pig?  ");
+            petType = petTypeScanner.nextLine();
+            if (Objects.equals(petType, "dog")) {
+                System.out.println("WHAT IS THE" + petType + "'s Name?");
+                petName = petNameScanner.nextLine();
+                Pet dog = new Dog(petName);
+                petList.add(dog);
+            }
+            if (Objects.equals(petType, "cat")) {
+                System.out.println("WHAT IS THE" + petType + "'s Name?");
+                petName = petNameScanner.nextLine();
+                Pet cat = new Cat(petName);
+                petList.add(cat);
+            }
+            if (Objects.equals(petType, "pig")) {
+                System.out.println("WHAT IS THE" + petType + "'s Name?");
+                petName = petNameScanner.nextLine();
+                Pet pig = new Pig(petName);
+                petList.add(pig);
+            }
+            StringBuilder petBuilder = new StringBuilder();
+            for (Pet j: petList){
+                petBuilder.append("\n Pet").append(j.getName()).append(j.speak());
+            }
+
+
         }
 
-
     }
-
 }
--- src/main/java/io/zipcoder/polymorphism/Pet.java
+++ src/main/java/io/zipcoder/polymorphism/Pet.java
@@ -1,6 +1,6 @@
 package io.zipcoder.polymorphism;
 
-public abstract class Pet {
+public abstract class Pet implements Animal{
 
     private String name;
 
